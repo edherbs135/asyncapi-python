223cdd2c46712e32b20bf3557c9c16bc96abf86a
Ed Herbert <edherbs147@gmail.com>
2022-02-09 12:31:02 -0500

    Initial commit

---
+++ asyncapi/__init__.py
@@ -0,0 +1,4 @@
+"""asyncapi-python - Library for generating AsyncAPI spec from python definitions, converting to YAML, JSON, HTML and spinning up consumers/producers etc"""
+
+__version__ = "0.1.0"
+__all__ = []  # type: ignore
